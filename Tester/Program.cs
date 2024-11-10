e413903ef00496c7f69398c7c0f2e2bd2fb8d0af
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-11-10 21:49:12 +1100

    Tests!

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,9 +1,27 @@
-﻿byte[]? test = null;
+﻿using NAudio.Wave;
+using OpusSharp.Core;
 
-unsafe
+var format = new WaveFormat(48000, 1);
+var buffer = new BufferedWaveProvider(format) { ReadFully = true };
+var encoder = new OpusEncoder(format.SampleRate, format.Channels, OpusPredefinedValues.OPUS_APPLICATION_VOIP);
+var decoder = new OpusDecoder(format.SampleRate, format.Channels);
+var recorder = new WaveInEvent() { BufferMilliseconds = 20, WaveFormat = format };
+var player = new WaveOutEvent();
+recorder.DataAvailable += Recorder_DataAvailable;
+
+recorder.StartRecording();
+player.Init(buffer);
+player.Play();
+
+void Recorder_DataAvailable(object? sender, WaveInEventArgs e)
 {
-    fixed (byte* ptr = test.AsSpan())
-    {
-        Console.WriteLine(new IntPtr(ptr));
-    }
+    var encoded = new byte[1000];
+    var encodedBytes = encoder.Encode(e.Buffer, 960, encoded, encoded.Length);
+    Console.WriteLine(encodedBytes);
+    var decoded = new byte[1920];
+    var decodedSamples = decoder.Decode(encoded, encodedBytes, decoded, 960, false);
+    Console.WriteLine(decodedSamples);
+    buffer.AddSamples(decoded, 0, decoded.Length);
 }
+
+Console.ReadLine();
